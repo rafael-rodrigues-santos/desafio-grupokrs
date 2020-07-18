a7f3a06e112470ed9c5191a41c432bc290a3156b
Rafael <rafaelrodrigues_@outlook.com>
2020-07-17 23:38:19 -0300

    Criada estrutura inicial 'adc filmes'

--- locadora/src/components/add/add.jsx
+++ locadora/src/components/add/add.jsx
@@ -5,12 +5,34 @@ export default class Add extends Component {
   render() {
     return (
       <div className="add">
-        <div className="add">
           <div className="add__title">
             <h1> Adcionar Filmes</h1>
           </div>
-          <div className="add__form"></div>
-        </div>
+          <div className="add__form">
+              <form className="form">
+                  <label> Título do Filme: </label>
+                  <input></input>
+                  <label> Sinopse: </label>
+                  <input></input>
+                  <label> Genero: </label>
+                  <input></input>
+                  <label> Data de lançamento: </label>
+                  <input></input>
+                  <label> Idioma: </label>
+                  <input></input>
+                  <label> Legendado: </label>
+                  <input></input>
+                  <label> Diretor: </label>
+                  <input></input>
+                  <label> Link no IMDB: </label>
+                  <input></input>
+                  <label> Avaliação do filme: </label>
+                  <input></input>
+                  <button> Cadastrar </button>
+                  <button> Limpar </button>
+              </form>
+          </div>
+        
       </div>
     );
   }
