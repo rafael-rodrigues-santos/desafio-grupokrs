16b6f0002c9de5c7a0ea8c06231f6ea190e77d12
Rafael <rafaelrodrigues_@outlook.com>
2020-07-18 18:06:31 -0300

    Testando listagem de filmes a partir do db

--- locadora/src/components/list/list.jsx
+++ locadora/src/components/list/list.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import "./list.css";
+import api from "../../api.js";
 
 export default class List extends Component {
+
+  state = {
+    filmes: [],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('/filmes');
+
+    this.setState({filmes: response.data});
+  }
+
   render() {
+
+    const {filmes} = this.state;
+    console.log("Teste", filmes);
+
     return (
       <div className="list">
           <div className="list_title">
             <h1> Lista de Filmes</h1>
           </div>
           <div className="list__content">
+          <ul>
+            {filmes && filmes.map(filme => (
+              <li key= {filme.id}>
+                Nome do filme: {filme.titulo}
+              </li>
+              )
+            )}
+          </ul>
               
           </div>
         
