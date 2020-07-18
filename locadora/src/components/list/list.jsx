0b455533124fcf6f9d88188747b32a6c753c110a
Rafael <rafaelrodrigues_@outlook.com>
2020-07-18 00:16:00 -0300

    Criada estrutura inicial 'listar filmes'

---
+++ locadora/src/components/list/list.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./list.css";
+
+export default class List extends Component {
+  render() {
+    return (
+      <div className="list">
+          <div className="list_title">
+            <h1> Lista de Filmes</h1>
+          </div>
+          <div className="list__content">
+              
+          </div>
+        
+      </div>
+    );
+  }
+}
