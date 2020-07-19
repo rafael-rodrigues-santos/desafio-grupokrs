c5c31633b016f93b03ae7ae5526fc4a6fbf0f577
Rafael <rafaelrodrigues_@outlook.com>
2020-07-19 12:15:24 -0300

    Adcionando 'Link' para navegar entre páginas

--- locadora/src/components/menu/menu.jsx
+++ locadora/src/components/menu/menu.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./menu.css";
+import { Link } from "react-router-dom";
 
 export default class Menu extends Component {
     render(){
@@ -8,8 +9,8 @@ export default class Menu extends Component {
                 <div className="menu__container">
                     <div className="menu__container-itens">
                         <ul>
-                            <li> Home </li>
-                            <li> Adcionar Filme </li>
+                            <li> <Link to="/list"> Home </Link> </li>
+                            <li> <Link to="/add"> Adcionar Filme </Link> </li>
                         </ul>
                     </div>
                 </div>
