9c053ecb282ed8f118b5043ef57fa04a44b739b2
Rafael <rafaelrodrigues_@outlook.com>
2020-07-19 11:56:52 -0300

    Testando 'adcionar filme'

--- locadora/src/components/add/add.jsx
+++ locadora/src/components/add/add.jsx
@@ -1,39 +1,131 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./add.css";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import api from "../../api.js";
 
-export default class Add extends Component {
-  render() {
-    return (
-      <div className="add">
-          <div className="add__title">
-            <h1> Adcionar Filmes</h1>
-          </div>
-          <div className="add__form">
-              <form className="form">
-                  <label> Título do Filme: </label>
-                  <input></input>
-                  <label> Sinopse: </label>
-                  <input></input>
-                  <label> Genero: </label>
-                  <input></input>
-                  <label> Data de lançamento: </label>
-                  <input></input>
-                  <label> Idioma: </label>
-                  <input></input>
-                  <label> Legendado: </label>
-                  <input></input>
-                  <label> Diretor: </label>
-                  <input></input>
-                  <label> Link no IMDB: </label>
-                  <input></input>
-                  <label> Avaliação do filme: </label>
-                  <input></input>
-                  <button> Cadastrar </button>
-                  <button> Limpar </button>
-              </form>
-          </div>
+const AddUser = () => {
+  let history = useHistory();
+  const [filme, setUser ] = useState({
+    titulo: "",
+    sinopse: ""
+  });
+
+  const { titulo, sinopse } = filme;
+      const onInputChange = e => {
+        console.log(e.target.value);
+        setUser({ ... filme, [e.target.name]: e.target.value });
         
-      </div>
-    );
-  }
-}
+      }
+
+      const onSubmit = async e => {
+        e.preventDefault();
+        await api.post("/filmes", filme);
+        // history.push('/');
+      } ;
+    
+      return (
+        <div className="add">
+            <div className="add__title">
+              <h1> Adcionar Filmes</h1>
+            </div>
+            <div className="add__form">
+                <form className="form" onSubmit={e => onSubmit(e)}>
+                    <label> Título do Filme: </label>
+                    <input type="text" name="titulo" value={titulo} onChange={e => onInputChange(e)} />
+                    <label> Sinopse: </label>
+                    <input type="text" name="sinopse" value={sinopse} onChange={e => onInputChange(e)} ></input>
+                    <label> Genero: </label>
+                    <input type="text"></input>
+                    <label> Data de lançamento: </label>
+                    <input type="text"></input>
+                    <label> Idioma: </label>
+                    <input type="text"></input>
+                    <label> Legendado: </label>
+                    <input type="text"></input>
+                    <label> Diretor: </label>
+                    <input type="text"></input>
+                    <label> Link no IMDB: </label>
+                    <input type="text"></input>
+                    <label> Avaliação do filme: </label>
+                    <input type="text"></input>
+                    <button> Cadastrar </button>
+                    {/* <button onClick={(e) => this.fSubmit(e)}> Cadastrar </button> */}
+                    <button> Limpar </button>
+                </form>
+            </div>
+          
+        </div>
+      );
+    }
+  export default AddUser;
+
+
+// export default class Add extends Component {
+
+  // constructor(props){
+  //   super(props);
+  //   this.state={
+  //     filmes: []
+  //   }
+  // }
+
+//   render() {
+//     const AddUser = () => {
+//       let history = useHistory();
+//       const [filme, setUser ] = useState({
+//         titulo: "",
+//         sinopse: ""
+//       });
+//     }
+  
+//       const { titulo, sinopse } = this.filme;
+//       const onInputChange = e => {
+//         console.log(e.target.value);
+//         setUser({ ... this.filme, [e.target.titulo]: e.target.value });
+//         history.push('/')
+//       }
+    
+    
+
+//     const onSubmit = async e => {
+//       e.preventDefault();
+//       await axios.post("http://localhost:4000/filmes", this.filme);
+
+//     } ;
+  
+//     return (
+//       <div className="add">
+//           <div className="add__title">
+//             <h1> Adcionar Filmes</h1>
+//           </div>
+//           <div className="add__form">
+//               <form className="form" onSubmit={e => onSubmit(e)}>
+//                   <label> Título do Filme: </label>
+//                   <input type="text" name="titulo" value={this.titulo} onChange={e => onInputChange(e)} />
+//                   <label> Sinopse: </label>
+//                   <input type="text" name="sinopse" value={this.sinopse} onChange={e => onInputChange(e)} ></input>
+//                   <label> Genero: </label>
+//                   <input type="text"></input>
+//                   <label> Data de lançamento: </label>
+//                   <input type="text"></input>
+//                   <label> Idioma: </label>
+//                   <input type="text"></input>
+//                   <label> Legendado: </label>
+//                   <input type="text"></input>
+//                   <label> Diretor: </label>
+//                   <input type="text"></input>
+//                   <label> Link no IMDB: </label>
+//                   <input type="text"></input>
+//                   <label> Avaliação do filme: </label>
+//                   <input type="text"></input>
+//                   <button> Cadastrar </button>
+//                   {/* <button onClick={(e) => this.fSubmit(e)}> Cadastrar </button> */}
+//                   <button> Limpar </button>
+//               </form>
+//           </div>
+        
+//       </div>
+//     );
+//   }
+// }
