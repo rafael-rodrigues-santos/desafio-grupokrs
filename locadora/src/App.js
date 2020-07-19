978fa9cb2885dfb942483da48f1406a1e056b42b
Rafael <rafaelrodrigues_@outlook.com>
2020-07-19 11:58:52 -0300

    Criando navegação entre páginas

--- locadora/src/App.js
+++ locadora/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Menu from '../src/components/menu/menu';
+import List from './components/list/list';
+import Add from './components/add/add';
 
 function App() {
   return (
     <div className="App">
-      <h1> Teste </h1>
+      <Router>
+
+
+      <Menu />
+
+    <Switch>
+      <Route exact path="/" component={List} />
+      <Route exact path="/list" component={List} />
+      <Route exact path="/add" component={Add} />
+    </Switch>
+      </Router>
     </div>
   );
 }
--- locadora/src/index.js
+++ locadora/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import Menu from '../src/components/menu/menu.jsx';
 import Add from '../src/components/add/add.jsx';
-import List from '../src/components/list/list.jsx';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Menu />
-    {/* <Add /> */}
-    <List />
+    <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
