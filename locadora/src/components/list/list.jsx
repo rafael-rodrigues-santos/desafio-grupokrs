735c278fee2a1bca989ac05bc7b93f35ad114a6a
Rafael <rafaelrodrigues_@outlook.com>
2020-07-19 16:26:39 -0300

    Alterado link do botão editar

--- locadora/src/components/list/list.jsx
+++ locadora/src/components/list/list.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./list.css";
 import api from "../../api.js";
+import { Link } from "react-router-dom";
 
 export default class List extends Component {
 
@@ -10,6 +11,7 @@ export default class List extends Component {
 
   async componentDidMount(){
     const response = await api.get('/filmes');
+    console.log(response.data);
 
     this.setState({filmes: response.data});
   }
@@ -25,6 +27,47 @@ export default class List extends Component {
             <h1> Lista de Filmes</h1>
           </div>
           <div className="list__content">
+            <table border='1'>
+              <th> ID </th>
+              <th> Título do Filme </th>
+              <th> Gênero </th>
+              <th> Data de Lançamento </th>
+              <th> Idioma </th>
+              <th> Legendado </th>
+              <th> Ação </th>
+              
+                  {filmes && filmes.map(filme => (
+                  <tr key= {filme.id}>
+                    <td>
+                      {filme.id}
+                    </td>
+                    <td>
+                      {filme.titulo}
+                    </td>
+                    <td>
+                      {filme.genero}
+                    </td>
+                    <td>
+                      {filme.lancamento}
+                    </td>
+                    <td>
+                      {filme.idioma}
+                    </td>
+                    <td>
+                      {filme.legenda}
+                    </td>
+                    <td>
+                      <button>Excluir</button>
+                      <button> <Link to={`filmes/${filme.id}`}> Edit </Link> </button>
+                    </td>
+                  </tr>
+                  )
+                )}
+              
+             
+
+            </table>
+
           <ul>
             {filmes && filmes.map(filme => (
               <li key= {filme.id}>
