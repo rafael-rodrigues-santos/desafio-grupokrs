1a80346f67f2cbadb239ba179789863b1832189f
Rafael <rafaelrodrigues_@outlook.com>
2020-07-21 00:03:42 -0300

    Remoção de espaços desnecessários

--- locadora/src/App.js
+++ locadora/src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Menu from '../src/components/menu/menu';
-import List from './components/list/list';
-import Add from './components/add/add';
-import Edit from './components/edit/edit';
-import Detail from './components/detail/detail';
+import Menu from "../src/components/menu/menu";
+import List from "./components/list/list";
+import Add from "./components/add/add";
+import Edit from "./components/edit/edit";
+import Detail from "./components/detail/detail";
 
 function App() {
   return (
     <div className="App">
       <Router>
-
-
-      <Menu />
-
-    <Switch>
-      <Route exact path="/" component={List} />
-      <Route exact path="/list" component={List} />
-      <Route exact path="/add" component={Add} />
-      <Route exact path="/filmes/list/:id" component={Edit} />
-      <Route exact path="/filmes/:id" component={Detail} />
-    </Switch>
+        <Menu />
+        <Switch>
+          <Route exact path="/" component={List} />
+          <Route exact path="/list" component={List} />
+          <Route exact path="/add" component={Add} />
+          <Route exact path="/filmes/list/:id" component={Edit} />
+          <Route exact path="/filmes/:id" component={Detail} />
+        </Switch>
       </Router>
     </div>
   );
